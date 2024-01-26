474c1e71fce55685ca63a4e0a9d882d2b13cfadc
PauHiga <p.gabriela.higa@gmail.com>
2024-01-26 10:25:03 -0300

    post sector endpoint works

---
+++ backend-java/src/main/java/com/example/demo/controllers/SectorController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.SectorDTO;
+import com.example.demo.dto.SupplierDTO;
+import com.example.demo.models.Sector;
+import com.example.demo.models.Supplier;
+import com.example.demo.services.SectorService;
+import com.example.demo.services.SupplierService;
+
+@RestController
+@RequestMapping("/sectors")
+@CrossOrigin(origins = "http://localhost:4200")
+public class SectorController {
+	
+	@Autowired
+	SectorService sectorService;
+
+	@GetMapping()
+	public ResponseEntity<List<Sector>> getSectors(){
+		return ResponseEntity.ok(sectorService.getSectores());
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Object> returnSupplier(@RequestBody SectorDTO sectorDTO){
+		try {
+			return ResponseEntity.ok(sectorService.createSector(sectorDTO));
+		}
+		catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating sector: " + e.getMessage());			
+		}
+	}
+
+	
+}
--- backend-java/src/main/java/com/example/demo/services/SectorService.java
+++ backend-java/src/main/java/com/example/demo/services/SectorService.java
@@ -1,9 +1,12 @@
 package com.example.demo.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.SectorDTO;
 import com.example.demo.models.Sector;
 import com.example.demo.repositories.SectorRepository;
 
@@ -20,8 +23,15 @@ public class SectorService {
 		return sectorRepository.findById(id);
 	}
 	
-	public Sector createSector(Sector sector) {
-		sectorRepository.save(sector);
+	public Sector createSector(SectorDTO sectorDTO) {
+		Sector sector = sectorRepository.findByName(sectorDTO.getName())
+				.orElseGet(()->{
+					Sector newSector =  new Sector();
+					newSector.setName(sectorDTO.getName());
+					newSector.setCreatedAt(LocalDate.now());
+					newSector.setDeleted(false);
+					return sectorRepository.save(newSector);
+				});
 		return sector;
 	}
 }
